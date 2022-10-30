62119af5e38a9c782e5f8736a987cf9fda98dafe
pg-7v <dmkseev@gmail.com>
2022-10-30 16:33:44 +0300

    feat: added CRUD, view, template and tests

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-print(STATIC_URL)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
